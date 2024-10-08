8327ea5331c518b3d059fae4d97075485f38a55c
Ibrahim Abdulmajeed <ibrahimabdulmajeed14@gmail.com>
2024-10-08 23:00:58 +0100

    upload portfolio web
    this is my portfolio web

---
+++ js/click-scroll.js
@@ -0,0 +1,49 @@
+//jquery-click-scroll
+//by syamsul'isul' Arifin
+
+var sectionArray = [1, 2, 3, 4, 5];
+
+$.each(sectionArray, function(index, value){
+          
+     $(document).scroll(function(){
+         var offsetSection = $('#' + 'section_' + value).offset().top - 90;
+         var docScroll = $(document).scrollTop();
+         var docScroll1 = docScroll + 1;
+         
+        
+         if ( docScroll1 >= offsetSection ){
+             $('.navbar-nav .nav-item .nav-link').removeClass('active');
+             $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');  
+             $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
+             $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
+         }
+         
+     });
+    
+    $('.click-scroll').eq(index).click(function(e){
+        var offsetClick = $('#' + 'section_' + value).offset().top - 90;
+        e.preventDefault();
+        $('html, body').animate({
+            'scrollTop':offsetClick
+        }, 300)
+    });
+    
+});
+
+(function ($) {
+    "use strict";
+  
+    $('.skill').waypoint(function () {
+      $('.progress .progress-bar').each(function () {
+          $(this).css("width", $(this).attr("aria-valuenow") + '%');
+      });
+    }, {offset: '80%'});
+  
+  
+  })(jQuery);
+
+$(document).ready(function(){
+    $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');    
+    $('.navbar-nav .nav-item .nav-link').eq(0).addClass('active');
+    $('.navbar-nav .nav-item .nav-link:link').eq(0).removeClass('inactive');
+});
